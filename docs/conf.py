f063fbe3da28d9f4a9cedc901ef7a9d5e751688a
Ashley Smith <ashley.smith@magneticearth.org>
2020-02-10 22:29:01 +0000

    Change to lighter logo.

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ html_theme_path = ["_themes", ]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = '_static/vre_logo.svg'
+html_logo = '_static/vre_logo_light.svg'
 # See https://sphinx-rtd-theme.readthedocs.io/en/latest/configuring.html
 html_theme_options = {
     'navigation_depth': 1
